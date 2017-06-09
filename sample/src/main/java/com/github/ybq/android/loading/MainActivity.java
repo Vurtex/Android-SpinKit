9851a2d8be36f4a1a3106d9468452d1dbe4cdc7d
Vurtex <hongkeshu@gmail.com>
2017-06-09 17:49:50 +0800

    fix

--- sample/src/main/java/com/github/ybq/android/loading/MainActivity.java
+++ sample/src/main/java/com/github/ybq/android/loading/MainActivity.java
@@ -34,6 +34,7 @@ public class MainActivity extends AppCompatActivity {
                 }
             }
 
+            
             @Override
             public int getCount() {
                 return 2;
